bea391f954e197a9b8fb6722eb62105746743ce1
its-me-ojas <crestfallenwizard@gmail.com>
2023-10-14 06:22:42 +0530

    add assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-export default function App(){
+export default function App() {
   return (
-    <h1 className="text-3xl text-slate-100 font-bold underline">
-      Hello World
+    <h1 className="text-3xl text-slate-900 font-bold underline">
+      Hello World!
     </h1>
   )
 }
